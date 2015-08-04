2b30ebf33f0e477927ae1c3d6ea142ff30abf353
remingm <remingm@students.wwu.edu>
2015-08-04 11:13:48 -0700

    Added imagesLoaded script to fix formatting errors when images load slowly.

--- js/agency.js
+++ js/agency.js
@@ -24,3 +24,26 @@ $('body').scrollspy({
 $('.navbar-collapse ul li a').click(function() {
     $('.navbar-toggle:visible').click();
 });
+
+
+
+///////////////
+
+// external js: masonry.pkgd.js, imagesloaded.pkgd.js
+
+docReady( function() {
+  // init Isotope
+  var grid = document.querySelector('.grid');
+
+  var msnry = new Masonry( grid, {
+    itemSelector: '.grid-item',
+    columnWidth: '.grid-sizer',
+    percentPosition: true
+  });
+
+  imagesLoaded( grid, function() {
+    // layout Masonry after each image loads
+    msnry.layout();
+  });
+
+});
