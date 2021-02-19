14bba8de1a0bf9508b5688ad71c82e6c293a2893
remingm <michael.remington@outlook.com>
2021-02-18 18:42:10 -0800

    Remove jekyll

--- Gemfile
+++
@@ -1,5 +0,0 @@
-source 'https://rubygems.org'
-gem 'github-pages', group: :jekyll_plugins
-gem "kramdown", ">= 2.3.0"
-gem "json", ">= 2.3.0"
-gem "nokogiri", ">= 1.10.8"
--- build.sh
+++
@@ -1,2 +0,0 @@
-docker run --rm -it -v "$PWD":/srv/jekyll -p 4000:4000 jekyll/jekyll jekyll build
-# docker run --rm -it -v "$PWD":/srv/jekyll -p 4000:4000 jekyll/jekyll jekyll serve
